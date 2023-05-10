c37634b96535ced5e133023da5d1643782865962
mattyhatch <mattyhatch6@gmail.com>
2023-05-10 12:35:47 -0700

    Made options text slightly bigger

--- client/src/components/ImageTypeDropdown/ImageTypeDropdown.js
+++ client/src/components/ImageTypeDropdown/ImageTypeDropdown.js
@@ -6,7 +6,7 @@ const ImageTypeDropdown = props => {
   return(
     <div className={classes.Dropdown}>
     <h1>Which type of crop is shown in the image?</h1>
-    <label htmlFor="availableOptions">Please choose one of the available options: </label>
+    <label className={classes.Options}htmlFor="availableOptions">Please choose one of the available options: </label>
     <select id="availableOptions" name="availableOptions" 
             value={props.selected || ""} onChange={props.onChange}>
             <option value=""></option>
